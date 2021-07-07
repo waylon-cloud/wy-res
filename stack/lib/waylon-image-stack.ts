debba6a85d20fba1f39f81ea478a676a893da0b7
aletheia <bianchi.luca@gmail.com>
2021-07-07 21:46:37 +0200

    :construction: Add new stack elements
    Add bucket, dynamodb table and lambda function

--- stack/lib/waylon-image-stack.ts
+++ stack/lib/waylon-image-stack.ts
@@ -1,9 +1,55 @@
+import {resolve} from 'path';
 import * as cdk from '@aws-cdk/core';
+import {Bucket} from '@aws-cdk/aws-s3';
+import {AttributeType, BillingMode, Table} from '@aws-cdk/aws-dynamodb';
+import {} from '@aws-cdk/aws-lambda';
+import {NodejsFunction} from '@aws-cdk/aws-lambda-nodejs';
+interface WaylonImageStackProps extends cdk.StackProps {
+  storageBucket?: Bucket;
+  resourceTable?: Table;
+  region?: string;
+  stage?: string;
+}
 
 export class WaylonImageStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(
+    scope: cdk.Construct,
+    id: string,
+    props: WaylonImageStackProps = {}
+  ) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const {storageBucket, resourceTable} = props;
+    let {region, stage} = props;
+    region = region ? region : this.region;
+    stage = stage ? stage : process.env.WAYLON_STAGE;
+
+    const bucket = storageBucket
+      ? storageBucket
+      : new Bucket(this, 'storage-bucket', {
+          bucketName: `waylon-image-storage-${this.account}-${region}-${stage}`,
+        });
+
+    const table = resourceTable
+      ? resourceTable
+      : new Table(this, 'resource-table', {
+          tableName: `waylon-resources-${region}-${stage}`,
+          partitionKey: {
+            name: 'pk',
+            type: AttributeType.STRING,
+          },
+          billingMode: BillingMode.PAY_PER_REQUEST,
+        });
+
+    const uploadRequestFunction = new NodejsFunction(
+      this,
+      'upload-request-function',
+      {
+        functionName: `waylon-upload-request-${stage}`,
+        description:
+          'Function to handle upload requests and upload URL generation',
+        entry: resolve(__dirname, '../../app/way-image/http/index.ts'),
+      }
+    );
   }
 }
